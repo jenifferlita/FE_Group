bea48d48e568ef64af8f085643e8ef2e2d13bea7
jenifferlita <s22110064@student.unklab.ac.id>
2023-10-17 09:31:44 +0800

    menambahkan Header dan Logo

--- tugas8/src/App.jsx
+++ tugas8/src/App.jsx
@@ -1,18 +1,22 @@
-import './App.css'
-import Home from './components/Home'
-import Search from './components/Home/Search'
-import CardInfo from './components/CardInfo'
+import "./App.css";
+import Home from "./components/Home";
+import Search from "./components/Home/Search";
+import CardInfo from "./components/CardInfo";
+import Header from "./components/Header";
+import Logo from "./components/Header/Logo";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
-    <Home/>
-    <CardInfo/>
-    <Search/>
+      <Home />
+      <CardInfo />
+      <Search />
+      <Header />
+      <Logo />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
